cc92755215ee16aec941ae54dd5e2bb527e32542
Parth <desaiparth08@gmail.com>
2018-07-06 00:02:29 +0530

    Add zb-cli application code
    Add zombiebattleground cli code. It can be used to quickly create account. We will be adding more functionality in it as we build contract

---
+++ cli/cmd/root.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/loomnetwork/go-loom/client"
+	"github.com/spf13/cobra"
+)
+
+var rootCmdArgs struct {
+	privateKeyFilePath *string
+	readURI            *string
+	writeURI           *string
+}
+
+var commonTxObjs struct {
+	privateKey []byte
+	contract   *client.Contract
+}
+
+func readKeyFile() error {
+	fileContents, err := ioutil.ReadFile(*rootCmdArgs.privateKeyFilePath)
+	if err != nil {
+		return fmt.Errorf("Unable to read private key from file: %s",
+			*rootCmdArgs.privateKeyFilePath)
+	}
+
+	decodeBuffer := make([]byte, len(fileContents))
+	bytesDecoded, err := base64.StdEncoding.Decode(decodeBuffer, fileContents)
+	if err != nil {
+		return fmt.Errorf("Invalid base64 content in private key file: %s",
+			*rootCmdArgs.privateKeyFilePath)
+	}
+
+	commonTxObjs.privateKey = decodeBuffer[:bytesDecoded]
+	return nil
+}
+
+func connectToRPC(readURI string, writeURI string) error {
+	rpcClient := client.NewDAppChainRPCClient("default", writeURI, readURI)
+
+	loomAddress, err := rpcClient.Resolve("ZombieBattleground")
+	if err != nil {
+		return fmt.Errorf("Unable to resolve RPC connection. RPC Error:%s", err.Error())
+	}
+
+	commonTxObjs.contract = client.NewContract(rpcClient, loomAddress.Local)
+
+	return nil
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "zb-cli",
+	Short: "ZombieBattleGround cli tool",
+	PersistentPreRunE: func(command *cobra.Command, args []string) error {
+		var err error
+
+		err = readKeyFile()
+		if err != nil {
+			return err
+		}
+
+		err = connectToRPC(*rootCmdArgs.readURI, *rootCmdArgs.writeURI)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func Execute() {
+	rootCmdArgs.privateKeyFilePath = rootCmd.PersistentFlags().StringP("key", "k", "", "Private key file path")
+	rootCmdArgs.readURI = rootCmd.PersistentFlags().StringP("readURI", "r", "", "Read URI for rpc")
+	rootCmdArgs.writeURI = rootCmd.PersistentFlags().StringP("writeURI", "w", "", "Write URI for rpc")
+
+	rootCmd.Execute()
+}
--- cli/main.go
+++ cli/main.go
@@ -1,6 +1,7 @@
 package main
 
-// TODO: create commandline to interact with the contract
-// Example can be found here https://github.com/loomnetwork/go-loom/tree/master/examples/cli
+import "github.com/loomnetwork/zombie_battleground/cli/cmd"
 
-func main() {}
+func main() {
+	cmd.Execute()
+}
