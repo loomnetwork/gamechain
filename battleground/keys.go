d54a09ac749688b115b5751f7762ff0683213924
Parth <desaiparth08@gmail.com>
2018-07-12 15:20:14 +0530

    Adding getDecks function
    Adding deck functionality that associate default deck with user on creating an account.

---
+++ battleground/keys.go
@@ -0,0 +1,23 @@
+package battleground
+
+import "strings"
+
+func InitDataKey() []byte {
+	return []byte("init")
+}
+
+func NewUserKeySpace(userId string) *UserKeySpace {
+	return &UserKeySpace{userId: strings.TrimSpace(userId)}
+}
+
+type UserKeySpace struct {
+	userId string
+}
+
+func (u *UserKeySpace) AccountKey() []byte {
+	return []byte("user:" + u.userId)
+}
+
+func (u *UserKeySpace) DecksKey() []byte {
+	return []byte("user:" + u.userId + ":decks")
+}
