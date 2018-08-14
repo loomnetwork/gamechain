3a686b252656de067cfcffbe50217055b2ea0efd
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2018-08-14 22:00:42 +0700

    change hero_id to int64

--- cli/cmd/create_deck.go
+++ cli/cmd/create_deck.go
@@ -45,5 +45,5 @@ func init() {
 	rootCmd.AddCommand(createDeckCmd)
 
 	createDeckCmd.Flags().StringVarP(&createDeckCmdArgs.userID, "userId", "u", "loom", "UserId of account")
-	createDeckCmd.Flags().StringVarP(&createDeckCmdArgs.value, "value", "v", "{\"hero_id\":\"1\", \"name\": \"NewDeck\", \"cards\": [ {\"card_name\": \"Banshee\", \"amount\": 2}, {\"card_name\": \"Breezee\", \"amount\": 1} ]}", "Deck data in serialized json format")
+	createDeckCmd.Flags().StringVarP(&createDeckCmdArgs.value, "value", "v", "{\"hero_id\":1, \"name\": \"NewDeck\", \"cards\": [ {\"card_name\": \"Pyromaz\", \"amount\": 2}, {\"card_name\": \"Burrrnn\", \"amount\": 1} ]}", "Deck data in serialized json format")
 }
