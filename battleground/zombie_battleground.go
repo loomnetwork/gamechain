5f1aae36a00c10b8a77c78c962fd26abb3ecf8a1
Parth <desaiparth08@gmail.com>
2018-08-15 11:42:59 +0530

    Fix deck validation

--- battleground/zombie_battleground.go
+++ battleground/zombie_battleground.go
@@ -192,10 +192,6 @@ func (z *ZombieBattleground) CreateDeck(ctx contract.Context, req *zb.CreateDeck
 		return nil, err
 	}
 
-	if err := validateDeckName(deckList.Decks, req.Deck); err != nil {
-		return nil, err
-	}
-
 	// allocate new deck id
 	var newDeckId int64 = 0
 	if len(deckList.Decks) != 0 {
@@ -206,6 +202,10 @@ func (z *ZombieBattleground) CreateDeck(ctx contract.Context, req *zb.CreateDeck
 		}
 
 		newDeckId++
+	}
+
+	if err := validateDeckName(deckList.Decks, req.Deck); err != nil {
+		return nil, err
 	}
 
 	req.Deck.Id = newDeckId
@@ -254,6 +254,10 @@ func (z *ZombieBattleground) EditDeck(ctx contract.Context, req *zb.EditDeckRequ
 		return err
 	}
 	if err := validateDeckHero(heroes.Heroes, req.Deck.HeroId); err != nil {
+		return err
+	}
+
+	if err := validateDeckName(deckList.Decks, req.Deck); err != nil {
 		return err
 	}
 
