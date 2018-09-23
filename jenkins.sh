53f28c9423d9c21fdc7a67798d9a29a0757c6af6
Matthew Campbell <hyper@hyperworks.nu>
2018-09-24 00:24:20 +0700

    fix build

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export GOPATH=`pwd`
+#export GOPATH=`pwd`
+export GOPATH=/var/lib/jenkins/workspace/zombie_battleground_pr
 export LOOM_VER=404
 
 mkdir -p $GOPATH/bin
@@ -8,7 +9,6 @@ export PATH=$PATH:$GOPATH/bin
 
 wget https://private.delegatecall.com/loom/linux/build-${LOOM_VER}/loom -O ${GOPATH}/bin/loom
 go get github.com/loomnetwork/go-loom
-cd ${GOPATH}/src/github.com/loomnetwork/zombie_battleground
 
 make deps
 make
