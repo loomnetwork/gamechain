80a4ae140483506871438e4c5856009978d178fe
Matthew Campbell <hyper@hyperworks.nu>
2018-09-24 00:30:41 +0700

    fix build

--- jenkins.sh
+++ jenkins.sh
@@ -8,6 +8,7 @@ mkdir -p $GOPATH/bin
 export PATH=$PATH:$GOPATH/bin:/var/lib/jenkins/workspace/zombie_battleground_pr/bin #TODO make these paths less hard coded
 
 wget https://private.delegatecall.com/loom/linux/build-${LOOM_VER}/loom -O ${GOPATH}/bin/loom
+chmod +x  ${GOPATH}/bin/loom
 go get github.com/loomnetwork/go-loom
 
 make deps
