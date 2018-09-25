ed3599406d1326e509c4b4e224a6e60f52182fed
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2018-09-25 17:47:34 +0700

    print hero id in get_deck cmd

--- cli/cmd/get_deck.go
+++ cli/cmd/get_deck.go
@@ -35,6 +35,7 @@ var getDeckCmd = &cobra.Command{
 		}
 		fmt.Printf("deck name: %v\n", result.Deck.Name)
 		fmt.Printf("deck id: %v\n", result.Deck.Id)
+		fmt.Printf("hero id: %v\n", result.Deck.HeroId)
 		for _, card := range result.Deck.Cards {
 			fmt.Printf("card_name: %s, amount: %d\n", card.CardName, card.Amount)
 		}
