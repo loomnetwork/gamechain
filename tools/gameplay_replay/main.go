dba9a9f9f8901750cdef05b3dfea35908cb47ad0
Siddhant Mutha <siddhant@loomx.io>
2018-10-03 16:57:59 +0530

    validate args and add os.exit

--- tools/gameplay_replay/main.go
+++ tools/gameplay_replay/main.go
@@ -18,18 +18,23 @@ import (
 var pubKeyHexString = "e4008e26428a9bca87465e8de3a8d0e9c37a56ca619d3d6202b0567528786618"
 
 func main() {
+	if len(os.Args) == 0 {
+		log.Error("GamePlay JSON file not provided")
+		os.Exit(1)
+	}
+
 	// read game replay json
 	f, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Error("error opening json file: ", err)
-		return
+		os.Exit(1)
 	}
 
 	var gameReplay zb.GameReplay
 	err = jsonpb.Unmarshal(f, &gameReplay)
 	if err != nil {
 		log.Error("error unmarshalling json: ", err)
-		return
+		os.Exit(1)
 	}
 
 	j, _ := json.Marshal(gameReplay)
@@ -45,14 +50,14 @@ func main() {
 	initFile, err := os.Open("init.json")
 	if err != nil {
 		log.WithError(err).Error("error opening init.json")
-		return
+		os.Exit(1)
 	}
 
 	var initRequest zb.InitRequest
 	err = jsonpb.Unmarshal(initFile, &initRequest)
 	if err != nil {
 		log.WithError(err).Error("error unmarshalling init.json")
-		return
+		os.Exit(1)
 	}
 
 	err = zbContract.Init(*fakeCtx, &initRequest)
@@ -69,7 +74,7 @@ func main() {
 	err = initialiseStates(*fakeCtx, zbContract, initialState)
 	if err != nil {
 		log.Error("error initialising state: ", err)
-		return
+		os.Exit(1)
 	}
 
 	// start replaying the actions and validate states after each transition
@@ -78,7 +83,7 @@ func main() {
 	err = replayAndValidate(*fakeCtx, zbContract, actionList[1:])
 	if err != nil {
 		fmt.Println("error while validating gameplay: ", err)
-		return
+		os.Exit(1)
 	}
 }
 
