cc1a1ccbc77e6679d7d2f8de007eb0ccd158058f
Siddhant Mutha <siddhant@loomx.io>
2018-11-09 11:17:10 +0530

    read deck data from file

--- cli/cmd/set_ai_decks.go
+++ cli/cmd/set_ai_decks.go
@@ -1,16 +1,17 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/gogo/protobuf/jsonpb"
 	"github.com/loomnetwork/gamechain/types/zb"
 	"github.com/loomnetwork/go-loom/auth"
 	"github.com/spf13/cobra"
 )
 
 var setAIDecksCmdArgs struct {
-	data    string
+	file    string
 	version string
 }
 
@@ -21,16 +22,25 @@ var setAIDecksCmd = &cobra.Command{
 		signer := auth.NewEd25519Signer(commonTxObjs.privateKey)
 		var deckList zb.DeckList
 
-		if err := json.Unmarshal([]byte(setAIDecksCmdArgs.data), &deckList); err != nil {
-			return err
+		if setAIDecksCmdArgs.file == "" {
+			return fmt.Errorf("filename not provided")
 		}
 
+		f, err := os.Open(setAIDecksCmdArgs.file)
+		if err != nil {
+			return fmt.Errorf("error reading file: %s", err.Error())
+		}
+		defer f.Close()
+
+		if err := new(jsonpb.Unmarshaler).Unmarshal(f, &deckList); err != nil {
+			return fmt.Errorf("error parsing JSON file: %s", err.Error())
+		}
 		req := &zb.SetAIDecksRequest{
 			Decks:   deckList.Decks,
 			Version: setAIDecksCmdArgs.version,
 		}
 
-		_, err := commonTxObjs.contract.Call("SetAIDecks", req, signer, nil)
+		_, err = commonTxObjs.contract.Call("SetAIDecks", req, signer, nil)
 		if err != nil {
 			return err
 		}
@@ -42,6 +52,6 @@ var setAIDecksCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(setAIDecksCmd)
 
-	setAIDecksCmd.Flags().StringVarP(&setAIDecksCmdArgs.data, "data", "d", "{\"hero_id\":1, \"name\": \"NewDeck\", \"cards\": [ {\"card_name\": \"Pyromaz\", \"amount\": 2}, {\"card_name\": \"Burrrnn\", \"amount\": 1} ]}", "Deck data in serialized json format")
+	setAIDecksCmd.Flags().StringVarP(&setAIDecksCmdArgs.file, "file", "f", "", "json file containing decks data")
 	setAIDecksCmd.Flags().StringVarP(&setAIDecksCmdArgs.version, "version", "v", "v1", "Version")
 }
