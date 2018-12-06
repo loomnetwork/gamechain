4b8a3397ca2068410b47a0e0158c0859621bbb5e
Siddhant Mutha <siddhant@loomx.io>
2018-12-06 16:45:20 +0530

    create gateway for gc and pc

--- oracle/config.go
+++ oracle/config.go
@@ -1,6 +1,15 @@
 package main
 
 type OracleConfig struct {
-	GameChainPrivateKeyPath   string
+	GameChainPrivateKeyPath string
+	GameChainReadURI        string
+	GameChainWriteURI       string
+	GameChainEventsURI      string
+	GameChainPollInterval   int
+
 	PlasmaChainPrivateKeyPath string
+	PlasmaChainReadURI        string
+	PlasmaChainWriteURI       string
+	PlasmaChainEventsURI      string
+	PlasmaChainPollInterval   int
 }
