464d11912973be4ffbf8bf07148f409699d5c64f
Siddhant Mutha <siddhant@loomx.io>
2018-12-06 18:17:11 +0530

    add comment

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -14,6 +14,17 @@ import (
 	"github.com/pkg/errors"
 )
 
+/*
+* create rpc clients and DappchainGateway for gamechain (gc) and plasmachain (pc)
+* 	TODO: use correct contract names in `Resolve`
+* open a websocket conn for events in `Run`
+* websocket event should call the DappchainGateway's process events method
+* http calls to loomauth:
+* 	I think the endpoint is `/user/reward`, not sure and need to confirm.
+* 	another gateway kind of thing for that?
+* write contract method in ZB contract that stores the data coming from the pc events
+* get config from file etc (have defaults too?)
+ */
 type Oracle struct {
 	cfg *OracleConfig
 
@@ -126,6 +137,7 @@ func (orc *Oracle) connect() error {
 			return errors.Wrap(err, "failed to create gc dappchain gateway")
 		}
 	}
+
 	if orc.pcGateway == nil {
 		pcDappClient := client.NewDAppChainRPCClient(orc.pcChainID, orc.cfg.PlasmaChainWriteURI, orc.cfg.PlasmaChainReadURI)
 		orc.pcGateway, err = ConnectToDAppChainGateway(pcDappClient, orc.gpcAddress, orc.pcSigner, orc.logger)
@@ -199,4 +211,23 @@ func LoadDappChainPrivateKey(path string) ([]byte, error) {
 	}
 
 	return privKey, nil
+}
+
+func ConnectToDAppChainGateway(
+	loomClient *client.DAppChainRPCClient, caller loom.Address, signer auth.Signer,
+	logger *loom.Logger,
+) (*DAppChainGateway, error) {
+	gatewayAddr, err := loomClient.Resolve("gateway")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to resolve Gateway Go contract address")
+	}
+
+	return &DAppChainGateway{
+		Address:          gatewayAddr,
+		LastResponseTime: time.Now(),
+		contract:         client.NewContract(loomClient, gatewayAddr.Local),
+		caller:           caller,
+		signer:           signer,
+		logger:           logger,
+	}, nil
 }
