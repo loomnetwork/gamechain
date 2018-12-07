449c01a1c89a54e6d3d65d911a1220bde0b61ac9
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2018-12-07 12:06:04 +0700

    add set experience and level to hero

--- battleground/zombie_battleground.go
+++ battleground/zombie_battleground.go
@@ -642,6 +642,64 @@ func (z *ZombieBattleground) AddHeroExperience(ctx contract.Context, req *zb.Add
 	return &zb.AddHeroExperienceResponse{HeroId: hero.HeroId, Experience: hero.Experience}, nil
 }
 
+func (z *ZombieBattleground) SetHeroExperience(ctx contract.Context, req *zb.SetHeroExperienceRequest) (*zb.SetHeroExperienceResponse, error) {
+	if req.Experience <= 0 {
+		return nil, fmt.Errorf("experience needs to be greater than zero")
+	}
+
+	heroList, err := loadHeroes(ctx, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	hero := getHeroById(heroList.Heroes, req.HeroId)
+	if hero == nil {
+		return nil, contract.ErrNotFound
+	}
+	hero.Experience = req.Experience
+
+	if err := saveHeroes(ctx, req.UserId, heroList); err != nil {
+		return nil, err
+	}
+
+	senderAddress := []byte(ctx.Message().Sender.Local)
+	emitMsgJSON, err := prepareEmitMsgJSON(senderAddress, req.UserId, "setHeroExperience")
+	if err == nil {
+		ctx.EmitTopics(emitMsgJSON, "zombiebattleground:setheroexperience")
+	}
+
+	return &zb.SetHeroExperienceResponse{HeroId: hero.HeroId, Experience: hero.Experience}, nil
+}
+
+func (z *ZombieBattleground) SetHeroLevel(ctx contract.Context, req *zb.SetHeroLevelRequest) (*zb.SetHeroLevelResponse, error) {
+	if req.Level <= 0 {
+		return nil, fmt.Errorf("level needs to be greater than zero")
+	}
+
+	heroList, err := loadHeroes(ctx, req.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	hero := getHeroById(heroList.Heroes, req.HeroId)
+	if hero == nil {
+		return nil, contract.ErrNotFound
+	}
+	hero.Level = req.Level
+
+	if err := saveHeroes(ctx, req.UserId, heroList); err != nil {
+		return nil, err
+	}
+
+	senderAddress := []byte(ctx.Message().Sender.Local)
+	emitMsgJSON, err := prepareEmitMsgJSON(senderAddress, req.UserId, "setHeroLevel")
+	if err == nil {
+		ctx.EmitTopics(emitMsgJSON, "zombiebattleground:setheroexlevel")
+	}
+
+	return &zb.SetHeroLevelResponse{HeroId: hero.HeroId, Level: hero.Level}, nil
+}
+
 func (z *ZombieBattleground) GetHeroSkills(ctx contract.StaticContext, req *zb.GetHeroSkillsRequest) (*zb.GetHeroSkillsResponse, error) {
 	heroList, err := loadHeroes(ctx, req.UserId)
 	if err != nil {
