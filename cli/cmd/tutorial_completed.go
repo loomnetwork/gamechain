9b03b19f1fbdf5a7ef8e314a056505b4e1475dfa
Siddhant Mutha <siddhant@loomx.io>
2018-12-18 11:44:24 +0530

    fix contract call

--- cli/cmd/tutorial_completed.go
+++ cli/cmd/tutorial_completed.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/loomnetwork/gamechain/types/zb"
-	loom "github.com/loomnetwork/go-loom"
 	"github.com/loomnetwork/go-loom/auth"
 	"github.com/spf13/cobra"
 )
@@ -18,16 +17,12 @@ var tutorialCompletedCmd = &cobra.Command{
 	Short: "complete the tutorial for a user and get reward",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		signer := auth.NewEd25519Signer(commonTxObjs.privateKey)
-		callerAddr := loom.Address{
-			ChainID: commonTxObjs.rpcClient.GetChainID(),
-			Local:   loom.LocalAddressFromPublicKey(signer.PublicKey()),
-		}
 
 		req := &zb.RewardTutorialCompletedRequest{
 			UserId: tutorialCompletedCmdArgs.userID,
 		}
 		var resp zb.RewardTutorialCompletedResponse
-		_, err := commonTxObjs.contract.Call("RewardTutorialCompleted", req, callerAddr, &resp)
+		_, err := commonTxObjs.contract.Call("RewardTutorialCompleted", req, signer, &resp)
 		if err != nil {
 			return err
 		}
