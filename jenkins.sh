8e3dcde5e3ebc79413e8a9fa08367d9957c872a1
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-01-17 12:53:59 +0700

    add gcoracle image

--- jenkins.sh
+++ jenkins.sh
@@ -13,6 +13,7 @@ cd ${GOPATH}/src/github.com/loomnetwork/gamechain
 make deps
 make
 make gamechain-logger
+make bin/gcoracledocker
 make test
 
 # Docker image for gamechain-logger
@@ -29,8 +30,20 @@ docker build -t $DOC_IMAGE -f Dockerfile .
 echo "pushing to google container registry"
 gcloud docker -- push  $DOC_IMAGE
 
+# Docker image for gamechain-oracle
+DOC_IMAGE_ORACLE=gcr.io/robotic-catwalk-188706/gamechain-oracle:$REV
+
+chmod +x bin/gcoracle
+
+echo "sending $DOC_IMAGE_ORACLE"
+docker build -t $DOC_IMAGE_ORACLE -f Dockerfile_gcoracle .
+
+echo "pushing to google container registry"
+gcloud docker -- push  $DOC_IMAGE_ORACLE
+
 echo "sed on k8s/${ENV}/deployment.yaml"
 sed -i 's/%REV%/'"$REV"'/g' k8s/${ENV}/deployment.yaml
 
 echo "kube apply deployment"
 kubectl apply -f k8s/${ENV}/deployment.yaml  --kubeconfig=/var/lib/jenkins/${ENV}_kube_config.yaml
+
