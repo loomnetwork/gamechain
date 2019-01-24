5af3e374a0d81d00e188af21e0d1776f51c6f8fe
Serhii Yolkin <serhii.yolkin@gmail.com>
2019-01-24 06:28:04 +0100

    Revert "Use instance IDs in Proto instead of full card data"

--- cli/cmd/send_action_cardplay.go
+++ cli/cmd/send_action_cardplay.go
@@ -32,7 +32,9 @@ var sendActionCardPlayCmd = &cobra.Command{
 				PlayerId:   sendActionCardPlayCmdArgs.userID,
 				Action: &zb.PlayerAction_CardPlay{
 					CardPlay: &zb.PlayerActionCardPlay{
-						Card: &zb.InstanceId{Id: sendActionCardPlayCmdArgs.cardPlayInstanceID},
+						Card: &zb.CardInstance{
+							InstanceId: &zb.InstanceId{Id: sendActionCardPlayCmdArgs.cardPlayInstanceID},
+						},
 					},
 				},
 			},
