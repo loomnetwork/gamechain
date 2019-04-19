a25e98f1c67887605a55f6f691ccbef71066a59a
Serhii Yolkin <serhii.yolkin@gmail.com>
2019-04-19 19:53:38 +0200

    update for latest go-loom

--- cli/cmd/update_oracle.go
+++ cli/cmd/update_oracle.go
@@ -13,19 +13,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var updateOracleCmdArgs struct {
+	URI     string
+	chainID string
+}
+
 var updateOracleCmd = &cobra.Command{
 	Use:   "update_oracle (new oracle) [old oracle]",
 	Short: "change the oracle or set initial oracle",
 	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		signer := auth.NewEd25519Signer(commonTxObjs.privateKey)
-		newOracle, err := cli.ResolveAddress(args[0])
+		newOracle, err := cli.ResolveAddress(args[0], updateOracleCmdArgs.chainID, updateOracleCmdArgs.URI)
 		if err != nil {
 			return errors.Wrap(err, "resolve new oracle address arg")
 		}
 		var oldOracle loom.Address
 		if len(args) > 1 {
-			oldOracle, err = cli.ResolveAddress(args[1])
+			oldOracle, err = cli.ResolveAddress(args[1], updateOracleCmdArgs.chainID, updateOracleCmdArgs.URI)
 			if err != nil {
 				return errors.Wrap(err, "resolve old oracle address arg")
 			}
@@ -55,4 +60,9 @@ var updateOracleCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(updateOracleCmd)
+
+	updateOracleCmd.Flags().StringVarP(&updateOracleCmdArgs.URI, "URI", "u", "http://localhost:46658", "Root URI for rpc and query")
+	updateOracleCmd.Flags().StringVarP(&updateOracleCmdArgs.chainID, "chainID", "c", "default", "Chain ID")
+
+	_ = updateOracleCmd.MarkFlagRequired("URI")
 }
